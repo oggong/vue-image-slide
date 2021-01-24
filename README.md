aa216e36e7221814e02d0a866fe80bb97f8b161c
oggong <49095304+oggong@users.noreply.github.com>
2021-01-24 23:11:29 +0900

    0124 commit

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # vue-image-slide
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+#### 2021 0124
+
+##### bootstrap 과 bootstrap-vue를 이용 하였습니다.
--- src/components/ImageSlide.vue
+++ src/components/ImageSlide.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="box" style="width:1100px; height: 600px; box-shadow: 0 0 25px -3px rgba(0,0,0, 0.4)">
+    <div class="box" style="width:1100px; height: 600px; box-shadow: 0 0 25px -3px rgba(0,0,0, 0.4);">
         <img class="w-100 h-100" :src="image"/>
     </div>
 </template>
 <script>
+    export default {
+        name: 'image-slide',
+        data() {
+            return {
+                // 사진 이미지 소스 경로
+                images: ['images/img1.jpg', 'images/img2.jpg'],
 
-export default {
-    name: 'image-slide',
-    data(){
-        return {
-            // 사진 이미지 소스 경로
-            images: ['images/img1.jpg','images/img2.jpg'],
+                // 사진 이미지
+                image: '',
 
-            // 사진 이미지
-            image: '',
-
-            // 이미지 인덱스
-            i: 0
-        }
-    },
-    mounted(){
-        this.slideShow();
-    },
-    methods: {
-        slideShow() {
-            this.image = this.images[this.i];
-
-            if(this.i < this.images.length - 1) {
-                this.i ++;
-            } else {
-                this.i = 0;
+                // 이미지 인덱스
+                i: 0
             }
-            setTimeout(() => {
-                this.slideShow();
-            }, 3000)
-           
+        },
+        mounted() {
+            this.slideShow();
+        },
+        methods: {
+            slideShow() {
+                this.image = this.images[this.i];
+
+                if (this.i < this.images.length - 1) {
+                    this.i++;
+                }
+                else {
+                    this.i = 0;
+                }
+                setTimeout(() => {
+                    this.slideShow();
+                }, 3000)
+
+            }
         }
     }
-}
 </script>
