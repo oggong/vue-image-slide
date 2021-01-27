338d4040a6463c5a2e0b9e9f503ba4584e504d84
oggong <49095304+oggong@users.noreply.github.com>
2021-01-27 22:12:16 +0900

    슬라이드 0127

--- src/components/testSlide.vue
+++ src/components/testSlide.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="test">
-
+        <b-container class="test_inner">
+            <b-row class="test_row">
+                
+            </b-row>
+        </b-container>
     </div>
 </template>
 <script>
