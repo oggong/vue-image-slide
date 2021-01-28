9504fc761a8e3106ff1142fdd62c8ffa78719852
oggong <49095304+oggong@users.noreply.github.com>
2021-01-28 21:34:32 +0900

    커밋

--- src/components/testSlide.vue
+++ src/components/testSlide.vue
@@ -2,7 +2,7 @@
     <div class="test">
         <b-container class="test_inner">
             <b-row class="test_row">
-                
+                <b-col class="test_col"></b-col>
             </b-row>
         </b-container>
     </div>
